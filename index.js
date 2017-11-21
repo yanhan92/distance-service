e6b95bd69871381d8746f821bd38d5b577865a9b
Yan Han TAN <yanhan92@gmail.com>
2017-11-21 23:13:31 +0800

    Why isnt it working?

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var bot = new Tgfancy (token, {
 	}
 });
 
-bot.setWebHook(externalURL + ':443/bot' + token)
+bot.setWebHook(externalURL + ':443/bot' + token);
 
 var datum_mbs = {latitude: 1.283448, longitude: 103.858977};
 var datum_sail = {latitude: 1.280889, longitude: 103.852609};
@@ -23,35 +23,36 @@ var datum_LPS = {latitude: 1.280392, longitude: 103.850435};
 var datum_bismilah = {latitude: 1.305061, longitude: 103.853657};
 
 bot.on("text", function(message) {
-  var fromId = message.from.id;
-  var str=message.text+'';
-  var token = str.split(", ");
-  var datum = {};
-  datum["latitude"] = token[0];
-  datum["longitude"]=token[1];
-  console.log(`latitude = ${token[0]}, longitude = ${token[1]}`);
-  var distance = geolib.getDistance(
-  		datum_mbs,
-  		datum
-  	);
-  bot.sendMessage(fromId, "Distance between you and Marina Bay Sands: "+distance+"m");
+	  console.log("Received message");
+	  var fromId = message.from.id;
+	  var str=message.text+'';
+	  var token = str.split(", ");
+	  var datum = {};
+	  datum["latitude"] = token[0];
+	  datum["longitude"]=token[1];
+	  console.log(`latitude = ${token[0]}, longitude = ${token[1]}`);
+	  var distance = geolib.getDistance(
+	  		datum_mbs,
+	  		datum
+	  	);
+	  bot.sendMessage(fromId, "Distance between you and Marina Bay Sands: "+distance+"m");
 
-  var distance = geolib.getDistance(
-  		datum_sail,
-  		datum
-  	);
-  bot.sendMessage(fromId, "Distance between you and The Sail: "+distance+"m");
+	  var distance = geolib.getDistance(
+	  		datum_sail,
+	  		datum
+	  	);
+	  bot.sendMessage(fromId, "Distance between you and The Sail: "+distance+"m");
 
-  var distance = geolib.getDistance(
-  		datum_LPS,
-  		datum
-  	);
-  bot.sendMessage(fromId, "Distance between you and Lau Pa Sat: "+distance+"m");
+	  var distance = geolib.getDistance(
+	  		datum_LPS,
+	  		datum
+	  	);
+	  bot.sendMessage(fromId, "Distance between you and Lau Pa Sat: "+distance+"m");
 
-  var distance = geolib.getDistance(
-  		datum_bismilah,
-  		datum
-  	);
-  bot.sendMessage(fromId, "Distance between you and Bismilah Briyani: "+distance+"m");
+	  var distance = geolib.getDistance(
+	  		datum_bismilah,
+	  		datum
+	  	);
+	  bot.sendMessage(fromId, "Distance between you and Bismilah Briyani: "+distance+"m");
 });
 
