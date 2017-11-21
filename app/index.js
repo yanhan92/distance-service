07f9c9b86f9703e77bc9305ed2ee66531a8d05f6
Yan Han TAN <yanhan92@gmail.com>
2017-11-21 22:09:51 +0800

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,21 @@
+var Tgfancy = require ('tgfancy');
+var fs = require('fs');
+var port = process.env.PORT || 8443;
+var host = process.env.HOST;
+var externalURL = process.env.CUSTOM_ENV_VARIABLE || 'https://changiairbot.herokuapp.com' ;
+
+const QPX_API_KEY = process.env.QPX_API_KEY;
+const Telegram_API_KEY =process.env.Telegram_API_KEY; 
+const options = { write: __dirname + '\\data'};
+var changiairbot = new Tgfancy (Telegram_API_KEY, {
+	webHook: {
+		port : port,
+		host : host
+	}
+});
+
+changiairbot.setWebHook(externalURL + ':443/bot' + Telegram_API_KEY);
+
+changiairbot.on("text", function(message){
+	changiairbot.sendMessage(message.chat.id , "Hi");
+});
